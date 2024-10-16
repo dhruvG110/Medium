32ef01d35613edf50ab21b19fe7546518888fce5
dhruvG110 <guptadhruv472@gmail.com>
2024-10-17 01:37:18 +0530

    Added frontend folder

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,7 +8,7 @@ import { blogRouter } from './routes/blog';
 // Create the main Hono app
 const app = new Hono();
 
-app.route('/api/v1/user/',userRouter)
+app.route('/api/v1/user',userRouter)
 app.route('/api/v1/blog',blogRouter)
 
 export default app;
--- backend/src/middlewares/authMiddleware.ts
+++
@@ -1,6 +0,0 @@
-import { Context, Next } from "hono";
-import { decode, sign, verify, jwt } from 'hono/jwt'
-// JWT Authentication Middleware
-export const authMiddleware = async (c: Context, next: Next) => {
-  
-};
